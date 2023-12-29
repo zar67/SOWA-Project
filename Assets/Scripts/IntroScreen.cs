a309c273430baa4f6f1edb807b1b4880d19d9a44
Zoe <zoerowbotham1@gmail.com>
2023-12-29 13:09:34 +0000

    Requesting permissions in intro screen

--- Assets/Scripts/IntroScreen.cs
+++ Assets/Scripts/IntroScreen.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+
 public class IntroScreen : MonoBehaviour
 {
     [SerializeField] private string m_mapSceneName;
 
     private void Awake()
     {
+#if UNITY_ANDROID
+        if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            MoveToMap();
+            return;
+        }
+
         PopupManager.Instance.OpenPopup(new PopupData()
         {
             Type = PopupType.DEFAULT,
@@ -16,8 +27,45 @@ public class IntroScreen : MonoBehaviour
             {
                 new PopupButtonData()
                 {
-                    Action = MoveToMap,
+                    Action = RequestLocationPermission,
                     Text = "Continue",
+                    CloseOnClick = true
+                }
+            }
+        });
+#else
+        MoveToMap();
+#endif
+    }
+
+    private void RequestLocationPermission()
+    {
+        var callbacks = new PermissionCallbacks();
+        callbacks.PermissionDenied += PermissionDenied;
+        callbacks.PermissionDeniedAndDontAskAgain += PermissionDenied;
+        callbacks.PermissionGranted += PermissionGranted;
+
+        Permission.RequestUserPermission(Permission.FineLocation, callbacks);
+    }
+
+    private void PermissionGranted(string _)
+    {
+        MoveToMap();
+    }
+
+    private void PermissionDenied(string _)
+    {
+        PopupManager.Instance.OpenPopup(new PopupData()
+        {
+            Type = PopupType.DEFAULT,
+            ShowCloseButton = false,
+            BodyText = "This app requires location permissions to work. Please enable location permissions in settings to enable this app to work.",
+            ButtonDatas = new PopupButtonData[]
+            {
+                new PopupButtonData()
+                {
+                    Action = () => Application.Quit(),
+                    Text = "Exit",
                     CloseOnClick = true
                 }
             }
