00277e0a87269fdad7b69dbdf4c6f3a4caf22167
Zoe <zoerowbotham1@gmail.com>
2023-12-29 13:29:19 +0000

    Adding reset button and updating coordinates display

---
+++ Assets/Scripts/CoordinatesDisplay.cs
@@ -0,0 +1,45 @@
+using Mapbox.Unity.Location;
+using Mapbox.Utils;
+using TMPro;
+using UnityEngine;
+
+public class CoordinatesDisplay : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI m_statusText;
+
+	private AbstractLocationProvider m_locationProvider = null;
+
+	void Update()
+	{
+		if (m_locationProvider == null)
+		{
+			m_locationProvider = LocationProviderFactory.Instance.DefaultLocationProvider as AbstractLocationProvider;
+		}
+
+		Location currentLocation = m_locationProvider.CurrentLocation;
+
+		if (currentLocation.IsLocationServiceInitializing)
+		{
+			m_statusText.text = "Initializing Location Services...";
+		}
+		else
+		{
+			if (!currentLocation.IsLocationServiceEnabled)
+			{
+				m_statusText.text = "Location Services not enabled";
+			}
+			else
+			{
+				if (currentLocation.LatitudeLongitude.Equals(Vector2d.zero))
+				{
+					m_statusText.text = "Waiting for location ....";
+				}
+				else
+				{
+					m_statusText.text = currentLocation.LatitudeLongitude.ToString();
+				}
+			}
+		}
+
+	}
+}
