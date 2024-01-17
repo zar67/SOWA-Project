ea22f71814d67bfb5591a6c83b9fc3c18b50df54
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2024-01-17 11:14:27 +0000

    Reset camera button hookup

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Camera m_targetCamera;
+    [SerializeField] private Button m_resetPositionButton;
 
+    private Vector3 m_defaultCameraPosition;
     private Vector3 m_touchStart;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        m_defaultCameraPosition = m_targetCamera.transform.position;
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        m_resetPositionButton.onClick.AddListener(ResetCameraPosition);
+    }
+
+    private void OnDisable()
+    {
+        m_resetPositionButton.onClick.RemoveListener(ResetCameraPosition);
+    }
+
     void Update()
     {
         var currentMousePosition = m_targetCamera.ScreenToWorldPoint(Input.mousePosition);
@@ -28,5 +39,10 @@ public class CameraController : MonoBehaviour
             Vector3 direction = m_touchStart - currentMousePosition;
             m_targetCamera.transform.position += direction;
         }
+    }
+
+    private void ResetCameraPosition()
+    {
+        m_targetCamera.transform.position = m_defaultCameraPosition;
     }
 }
