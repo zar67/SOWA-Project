8013f383c8f4cf359bddfb725dd1c140bb7acc60
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2024-01-17 12:02:35 +0000

    Adding zooming potential

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +6,13 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Camera m_targetCamera;
     [SerializeField] private Button m_resetPositionButton;
 
+    [Header("Zoom Config")]
+    [SerializeField] private float m_zoomMinSize = 20;
+    [SerializeField] private float m_zoomMaxSize = 60;
+    [SerializeField] private float m_zoomScaleFactor = 10;
+
     private Vector3 m_defaultCameraPosition;
+    private float m_defaultCameraZoom = 40;
     private Vector3 m_touchStart;
 
     private void Awake()
@@ -28,21 +32,51 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        var currentMousePosition = m_targetCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 currentMousePosition = m_targetCamera.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             m_touchStart = currentMousePosition;
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount == 2)
         {
-            Vector3 direction = m_touchStart - currentMousePosition;
-            m_targetCamera.transform.position += direction;
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPreviousPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePreviousPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPreviousPos - touchOnePreviousPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+            HandleZoom(difference * 0.01f);
         }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 direction = m_touchStart - currentMousePosition;
+                m_targetCamera.transform.position += direction;
+            }
+
+            float mouseScrollInput = Input.GetAxis("Mouse ScrollWheel");
+            if (mouseScrollInput != 0)
+            {
+                HandleZoom(mouseScrollInput * m_zoomScaleFactor);
+            }
+        }
+    }
+
+    private void HandleZoom(float increment)
+    {
+        Debug.Log(m_targetCamera.orthographicSize + ", " + increment);
+        m_targetCamera.orthographicSize = Mathf.Clamp(m_targetCamera.orthographicSize - increment, m_zoomMinSize, m_zoomMaxSize);
     }
 
     private void ResetCameraPosition()
     {
         m_targetCamera.transform.position = m_defaultCameraPosition;
+        m_targetCamera.orthographicSize = m_defaultCameraZoom;
     }
 }
