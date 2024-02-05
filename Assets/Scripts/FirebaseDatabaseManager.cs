3919887b4a6d7aba41013da5de874cf0197d88a5
Zoe <zoerowbotham1@gmail.com>
2024-02-05 19:15:08 +0000

    Storing litter to database on UI pressed

---
+++ Assets/Scripts/FirebaseDatabaseManager.cs
@@ -0,0 +1,61 @@
+using Firebase.Database;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class FirebaseDatabaseManager : SingletonMonoBehaviour<FirebaseDatabaseManager>
+{
+    [SerializeField] private FirebaseManager m_firebaseManager;
+
+    private FirebaseDatabase m_firebaseDatabase;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        m_firebaseDatabase = FirebaseDatabase.DefaultInstance;
+    }
+
+    public void AddValueChangedListener(string path, EventHandler<ValueChangedEventArgs> callback)
+    {
+        m_firebaseDatabase.GetReference(path).ValueChanged += callback;
+    }
+
+    public void RemoveValueChangedListener(string path, EventHandler<ValueChangedEventArgs> callback)
+    {
+        m_firebaseDatabase.GetReference(path).ValueChanged -= callback;
+    }
+
+
+    public void SaveData<T>(string path, T data)
+    {
+        m_firebaseDatabase.GetReference(path).SetRawJsonValueAsync(JsonUtility.ToJson(data));
+    }
+
+    public void AppendData<T>(string path, T data)
+    {
+        m_firebaseDatabase.GetReference(path).Push().SetValueAsync(JsonUtility.ToJson(data));
+    }
+
+    public async Task<T> LoadData<T>(string path)
+    {
+        DataSnapshot dataSnapshot = await m_firebaseDatabase.GetReference(path).GetValueAsync();
+        if (!dataSnapshot.Exists)
+        {
+            return default;
+        }
+
+        return JsonUtility.FromJson<T>(dataSnapshot.GetRawJsonValue());
+    }
+
+    public async Task<bool> DoesDataExist(string path)
+    {
+        DataSnapshot dataSnapshot = await m_firebaseDatabase.GetReference(path).GetValueAsync();
+        return dataSnapshot.Exists;
+    }
+
+    public void DeleteData(string path)
+    {
+        m_firebaseDatabase.GetReference(path).RemoveValueAsync();
+    }
+}
---
+++ Assets/Scripts/LitterData.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class LitterData
+{
+    public string Timestamp;
+    public string Location;
+}
