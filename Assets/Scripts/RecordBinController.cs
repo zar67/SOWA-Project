64adc7e3641b7aba5c41fedb4ec65cff455504a5
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2024-04-22 21:44:24 +0100

    Bin location requesting

---
+++ Assets/Scripts/RecordBinController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordBinController : MonoBehaviour
+{
+    [SerializeField] private Button m_binButton;
+
+    private void OnEnable()
+    {
+        m_binButton.onClick.AddListener(HandleRecordBinClicked);
+    }
+
+    private void OnDisable()
+    {
+        m_binButton.onClick.RemoveListener(HandleRecordBinClicked);
+    }
+
+    private void HandleRecordBinClicked()
+    {
+        PopupManager.Instance.OpenPopup(new GenericInfoPopupData()
+        {
+            BodyText = "By recording a bin location you are helping others find the easier way to dispose of any litter they find. Please note that bin locations will not update immediately.",
+            ShowCloseButton = true,
+            Type = PopupType.GENERIC_INFO,
+            ButtonDatas = new PopupButtonData[]
+            {
+                new PopupButtonData()
+                {
+                    Text = "Record New Bin",
+                    CloseOnClick = true,
+                    Action = BinRecordingManager.Instance.RequestNewBinLocation
+                }
+            }
+        });
+    }
+}
+
