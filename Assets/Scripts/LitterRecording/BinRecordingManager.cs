bd415749b662811542be99386f51b3c7084f060d
Zoe <zoerowbotham1@gmail.com>
2024-07-13 14:54:21 +0100

    Adding back bin recording - but to firebase instead

--- Assets/Scripts/LitterRecording/BinRecordingManager.cs
+++ Assets/Scripts/LitterRecording/BinRecordingManager.cs
@@ -1,26 +1,10 @@
 using Mapbox.Unity.Location;
-using System;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.Networking;
 
 public class BinRecordingManager : SingletonMonoBehaviour<BinRecordingManager>
 {
-    private string m_myId = "zar67";
-    private string m_accessToken = "sk.eyJ1IjoiemFyNjciLCJhIjoiY2x2YXV5b2hzMDI1dDJwcnN5aDByZHl5ZSJ9.ffdUioOHqd9oRxV97pklnw";
-    private string m_datasetId = "clvbdf3kb1xk31trty0dc9x5w";
-    private string m_tilesetId = "Bins";
+    public const string BINS_KEY = "MISSING_BINS_DATA";
 
     private AbstractLocationProvider m_locationProvider = null;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        Mapbox.Unity.MapboxAccess.Instance.ClearAllCacheFiles();
-
-        //StartCoroutine(ApplyDatasetToTileset());
-    }
 
     public void RequestNewBinLocation()
     {
@@ -30,39 +14,6 @@ public class BinRecordingManager : SingletonMonoBehaviour<BinRecordingManager>
         }
 
         Location currentLocation = m_locationProvider.CurrentLocation;
-
-        StartCoroutine(PostNewDatasetFeature(currentLocation.LatitudeLongitude.x, currentLocation.LatitudeLongitude.y));
-    }
-
-    IEnumerator PostNewDatasetFeature(double lat, double longitude)
-    {
-        string postURL = $"https://api.mapbox.com/datasets/v1/{m_myId}/{m_datasetId}/features/{Guid.NewGuid()}?access_token={m_accessToken}";
-
-        string geoJSONstring2 = $"{{\"type\":\"Feature\",\"properties\":{{}},\"geometry\":{{\"type\":\"Point\",\"coordinates\":[{longitude},{lat}]}}}}";
-
-        var www = UnityWebRequest.Put(postURL, geoJSONstring2);
-        www.SetRequestHeader("Content-Type", "application/json");
-        yield return www.SendWebRequest();
-
-        if (www.result != UnityWebRequest.Result.Success)
-        {
-            Debug.LogError("www error: " + www.error);
-        }
-    }
-
-    IEnumerator ApplyDatasetToTileset()
-    {
-        string postURL = $"https://api.mapbox.com/uploads/v1/{m_myId}?access_token={m_accessToken}";
-
-        string datasetTilesetInfo = $"{{\"tileset\":\"{m_myId}.{m_tilesetId}\",\"name\":\"{m_tilesetId}\",\"url\":\"mapbox://datasets/{m_myId}/{m_datasetId}\"}}";
-
-        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(datasetTilesetInfo);
-
-        var www = UnityWebRequest.Put(postURL, bytes);
-        www.SetRequestHeader("content-type", "application/json");
-
-        www.method = "POST"; // Hacked as Unity is weird.
-
-        yield return www.SendWebRequest();
+        FirebaseDatabaseManager.Instance.AppendData(BINS_KEY, $"{currentLocation.LatitudeLongitude.x},{currentLocation.LatitudeLongitude.y}");
     }
 }
